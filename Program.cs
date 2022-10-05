28693e2aa432ecc9dd0bf7dbe1da3c695a38b9f2
Jason D'Oyley <jdfdoyley@gmail.com>
2022-10-05 07:35:17 -0400

    Add intro text

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/********************************************************************
+ * Name: Jason D'Oyley
+ * Date:
+ * Assignment CIS214 Week 1 GP - Display Text
+ *
+ * Main application (program) class.
+ * In this application we will print a line of text using WriteLine
+ * and print a line of formatted text using Write
+ *
+ */
+public class DisplayText
+{
+    static void Main(string[] args)
+    {
+        // Print a line of text to the console using WriteLine
+        Console.WriteLine("Hello, Jason! Welcome to C# programming!");
+
+        // Print a blank line using WriteLine
+        Console.WriteLine();
+    }
+}
